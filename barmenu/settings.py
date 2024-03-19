9b861d3deb9ccf68cf049cbc8f1cfc74cbae6810
idoshita <koshiroot@yahoo.co.jp>
2024-03-19 15:48:04 +0900

    な

--- barmenu/settings.py
+++ barmenu/settings.py
@@ -124,17 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# 正しい処理？？
-# STATIC_ROOT = BASE_DIR("static")
-# STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR("static")
+STATIC_URL = "/static/"
 
-# MEDIA_ROOT = BASE_DIR("media")
-# MEDIA_URL = "/media/"
-
-
-STATIC_URL = "static/"
-
-
+MEDIA_ROOT = BASE_DIR("media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
